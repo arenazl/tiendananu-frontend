2c3c4cc08b0b347d863d1d6714bf91e9cbf391ec
Lucas Ezequiel Mariano Arenaz <LArenaz@bpba.com.ar>
2025-07-21 03:50:57 -0300

    Actualización del frontend

--- config.prod.js
+++ config.prod.js
@@ -1,2 +1,2 @@
 // Production configuration
-const API_BASE_URL = 'https://your-app-name.herokuapp.com/api'; // Reemplaza con tu URL de Heroku
+const API_BASE_URL = 'https://tiendananuajuares-api-3de86a59b55e.herokuapp.com/api'; // Reemplaza con tu URL de Heroku
